ae2ecaf20f6693d84db6b16c3ed2afbb0667b998
onwk <onwipa24@gmail.com>
2021-11-23 08:46:04 +0700

    password match decoder

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UserInputError } from 'apollo-server-errors';
 import loginInput from './dto/login.input';
+import { ForbiddenError } from 'apollo-server-express';
 // import { ForbiddenError } from 'apollo-server-errors';
 
 @Injectable()
@@ -14,23 +15,23 @@ export class AuthService {
     async validateUser(loginInput: loginInput): Promise<any>{
         const user = await this.usersService.findByEmail(loginInput.email); //find if recieved email is exists
 
-        // const passwordMatch = await bcrypt.compare(password, user.password);//compare recieved password with password in DB
+        const passwordMatch = await bcrypt.compare(loginInput.password, user.password);//compare recieved password with password in DB
 
         //then check if it's correct password
-        // if(user && passwordMatch){
-        //         const {password, ...rest} = user;
-        //         return rest; //
-        // }else {
-        //     throw new ForbiddenError('Incorrect pasword');
-        // }
-
-        if(user && user.password === loginInput.password){
-            const {password, ...rest} = user;
-                    return rest;
+        if(user && passwordMatch){
+                const {password, ...rest} = user;
+                return rest; //
+        }else {
+            throw new ForbiddenError('Incorrect pasword');
         }
 
-        //if do not find user or password does not exist
-        return null;
+        // if(user && user.password === loginInput.password){
+        //     const {password, ...rest} = user;
+        //             return rest;
+        // }
+
+        // //if do not find user or password does not exist
+        // return null;
     }
 
     async login(loginInput: loginInput){
