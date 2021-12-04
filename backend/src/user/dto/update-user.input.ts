c9c16fb4ff257062cda9c14d87e5f73799539622
onwk <onwipa24@gmail.com>
2021-12-04 14:45:48 +0700

    update user input

--- backend/src/user/dto/update-user.input.ts
+++ backend/src/user/dto/update-user.input.ts
@@ -5,4 +5,13 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 export class UpdateUserInput extends PartialType(CreateUserInput) {
   @Field(() => Int)
   id: number;
+  
+  @Field()
+  job: string;
+
+  @Field()
+  departmant: string;
+
+  @Field()
+  organization: string;
 }
