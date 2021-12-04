9925d9625863eb19f95b71564141823f12bb51d5
onwk <onwipa24@gmail.com>
2021-12-04 22:28:02 +0700

    gql auth guard

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -3,6 +3,8 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -15,26 +17,31 @@ export class UserResolver {
   }
 
   @Query(() => [User], { name: 'users' })
+  @UseGuards(GqlAuthGuard)
   findAll() {
     return this.userService.findAll();
   }
 
   @Query(() => User, { name: 'user' })
+  @UseGuards(GqlAuthGuard)
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.userService.findOne(id);
   }
 
   @Query(() => User, { name: 'email' })
+  @UseGuards(GqlAuthGuard)
   findByEmail(@Args('email', { type: () => String }) email: string) {
     return this.userService.findByEmail(email);
   }
 
   @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
   updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
     return this.userService.update(updateUserInput.id, updateUserInput);
   }
 
   @Mutation(() => User)
+  @UseGuards(GqlAuthGuard)
   removeUser(@Args('id', { type: () => Int }) id: number) {
     return this.userService.remove(id);
   }
