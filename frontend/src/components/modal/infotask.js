bcdd7a83d119b3d2a912ba43d128e81dc36fdd54
Taaleww <nantakarn.1.01@gmail.com>
2021-12-08 09:11:18 +0700

    FINISHED

--- frontend/src/components/modal/infotask.js
+++ frontend/src/components/modal/infotask.js
@@ -1,7 +1,7 @@
 function StatusTag({ status }) {
   if (status === 1) {
     return (
-      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
         TO DO
       </span>
     );
